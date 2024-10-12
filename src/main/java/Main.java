86f3266f1d567ec3e08b21a92d5d395f286ffd7f
ggrim22 <gtgrim22@gmail.com>
2024-10-12 17:46:36 -0400

    Edit print statement to main class for console.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,6 @@ public class Main {
         System.out.println("Input a number 1-3000 to convert to roman numeral: ");
         int input = scanner.nextInt();
         IntegerToRoman integerToRoman = new IntegerToRoman();
-        System.out.println(integerToRoman.integerToRomanNumeral(input));
+        System.out.println("Roman Numeral: " + integerToRoman.integerToRomanNumeral(input));
     }
 }
